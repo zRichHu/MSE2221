28b07c16153300d8c20eefb693eb3f9d87a50aba
zRichHu <157316173+zRichHu@users.noreply.github.com>
2025-02-07 12:26:36 -0500

    Update Tutorial4Q2.m
    Fixed Eqn

--- Tutorial 4/Tutorial4Q2.m
+++ Tutorial 4/Tutorial4Q2.m
@@ -6,4 +6,4 @@ V = 8;
 
 syms h;
 
-vpasolve(V == ((r^2) * acos((r-h) / r) - (r-h) * (sqrt(2 * r * h - (h^2))) * L), h)
+vpasolve((r^2 * acos((r-h) / r) - (r-h) * (sqrt(2 * r * h - (h^2)))) * L - V, h)
